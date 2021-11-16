c3cf817f2708bbe95d7bb95ab17446292023d47e
Luis Barca <luisbarcapons@gmail.com>
2021-11-16 11:02:37 +0100

    Detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
-/*===== MENU SHOW =====*/ 
+/*===== VISUALIZACIÓN MENU =====*/ 
 const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
 
-    if(toggle && nav){
+    if (toggle && nav) {
         toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
+            nav.classList.toggle('show');
+        });
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu');
 
-/*===== ACTIVE AND REMOVE MENU =====*/
+/* ===== ATIVAR Y ELIMINAR EL MENU ===== */
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Active link*/
+  /* Activar link */
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
   
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+  /* Eliminamos el menu móvil */
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+/* ===== SCROLL ANIMATION ===== */
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -33,25 +33,25 @@ const sr = ScrollReveal({
     reset: true
 });
 
-/*SCROLL HOME*/
+/*SCROLL INCIO*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
-/*SCROLL ABOUT*/
+/*SCROLL SOBRE MI*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 400}); 
 sr.reveal('.about__text',{delay: 400}); 
 
-/*SCROLL SKILLS*/
+/*SCROLL HABILIDADES*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
-/*SCROLL WORK*/
+/*SCROLL EXPERIENCIA*/
 sr.reveal('.work__img',{interval: 200}); 
 
-/*SCROLL CONTACT*/
+/*SCROLL CONTACT0*/
 sr.reveal('.contact__input',{interval: 200});
