f3d05fc05c70788b8a8e6a1d8801dd4d0586c343
Luis Barca <luisbarcapons@gmail.com>
2021-11-19 10:16:42 +0100

    Favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,41 +25,41 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*=============== ACCORDION ===============*/
-const accordionItems = document.querySelectorAll('.accordion__item')
+/* ===== ACCORDION ===== */
+const accordionItems = document.querySelectorAll('.accordion__item');
 
-// 1. Selecionar cada item
-accordionItems.forEach((item) =>{
-    const accordionHeader = item.querySelector('.accordion__header')
+// Selecionar cada item
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.accordion__header');
 
-    // 2. Seleccionar cada click del header
-    accordionHeader.addEventListener('click', () =>{
-        // 7. Crear la variable
-        const openItem = document.querySelector('.accordion-open')
+    // Seleccionar cada click del header
+    accordionHeader.addEventListener('click', () => {
+        // Crear la variable
+        const openItem = document.querySelector('.accordion-open');
         
-        // 5. Llamar a la funcion toggle item
-        toggleItem(item)
+        // Llamar a la funcion toggle item
+        toggleItem(item);
 
-        // 8. Validar si existe la clase
-        if(openItem && openItem!== item){
-            toggleItem(openItem)
+        // Validar si existe la clase
+        if (openItem && openItem!== item) {
+            toggleItem(openItem);
         }
     })
 })
 
-// 3. Crear una funcion tipo constante
-const toggleItem = (item) =>{
-    // 3.1 Crear la variable
-    const accordionContent = item.querySelector('.accordion__content')
+// Crear una funcion tipo constante
+const toggleItem = (item) => {
+    // Crear la variable
+    const accordionContent = item.querySelector('.accordion__content');
 
-    // 6. Si existe otro elemento que contenga la clase accorion-open que remueva su clase
-    if(item.classList.contains('accordion-open')){
-        accordionContent.removeAttribute('style')
-        item.classList.remove('accordion-open')
+    // Si existe otro elemento que contenga la clase accorion-open que remueva su clase
+    if(item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style');
+        item.classList.remove('accordion-open');
     }else{
-        // 4. Agregar el height maximo del content
-        accordionContent.style.height = accordionContent.scrollHeight + 'px'
-        item.classList.add('accordion-open')
+        // Agregar el height maximo del content
+        accordionContent.style.height = accordionContent.scrollHeight + 'px';
+        item.classList.add('accordion-open');
     }
 }
 
@@ -72,26 +72,26 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL INCIO*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__title', {}); 
+sr.reveal('.button', {delay: 200}); 
+sr.reveal('.home__img', {delay: 400}); 
+sr.reveal('.home__social-icon', { interval: 200}); 
 
 /*SCROLL SOBRE MI*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__img', {}); 
+sr.reveal('.about__subtitle', {delay: 400}); 
+sr.reveal('.about__text', {delay: 400}); 
 
 /*SCROLL HABILIDADES*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__subtitle', {}); 
+sr.reveal('.skills__text', {}); 
+sr.reveal('.skills__data', {interval: 200}); 
+sr.reveal('.skills__img', {delay: 600});
 
 /*SCROLL EXPERIENCIA*/
-sr.reveal('.work-subtitle',{interval: 200});
+sr.reveal('.work-subtitle', {interval: 200});
 sr.reveal('.work__table', {interval: 400}); 
 sr.reveal('.accordion__item', {interval: 200});
 
 /*SCROLL CONTACT0*/
-sr.reveal('.contact__input',{interval: 200});
+sr.reveal('.contact__input', {interval: 200});
