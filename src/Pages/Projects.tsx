9d83ea04b9f8ee32d75a009de06cbd82009036b8
Luisbp27 <luisbarcapons@gmail.com>
2025-04-06 22:49:11 +0200

    adding cloudinary for pictures to fix vercel bug

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -38,28 +38,28 @@ const projectsData = [
     {
         title: "Chocolate Sales Analysis",
         description: "Explore sales behavior, profitability, and commercial segmentation using clustering and PCA.",
-        image: "../../assets/images/choco_sales.png",
+        image: "https://res.cloudinary.com/luisbp27/image/upload/v1743971902/portfolio/mg9m3vvxazy80jpihxbu.jpg",
         tags: ["Python", "Data Science", "Economics"],
         link: "https://github.com/Luisbp27/chocoSales"
     },
     {
         title: "Titanic: Machine Learning from Disaster",
         description: "Which are the type of passengers that survived? There is any pattern?",
-        image: "../../assets/images/titanic.png",
+        image: "https://res.cloudinary.com/luisbp27/image/upload/v1743971904/portfolio/npdjls0umsxysbgdpgva.png",
         tags: ["Python", "Machine Learning", "Data Science"],
         link: "https://github.com/Luisbp27/practica2_ia",
     },
     {
         title: "Bash clone",
         description: "A simple clone of the bash shell",
-        image: "../../assets/images/bash.png",
+        image: "https://res.cloudinary.com/luisbp27/image/upload/v1743971902/portfolio/tptxf7so3vflmsdndpop.png",
         tags: ["C", "Linux", "Operating System"],
         link: "https://github.com/Luisbp27/bash"
     },
     {
         title: "Housing Price Prediction",
         description: "Predict the price of a house based on its features",
-        image: "../../assets/images/housing.png",
+        image: "https://res.cloudinary.com/luisbp27/image/upload/v1743971902/portfolio/jlpnusopimv54bsyhjwc.png",
         tags: ["Python", "Machine Learning", "Data Science"],
         link: "https://github.com/Luisbp27/housePredict"
     },
